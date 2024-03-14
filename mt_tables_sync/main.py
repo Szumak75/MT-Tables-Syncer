0b9b52b58aee03db136e8c544a5a0caa26005ca1
Jacek Kotlarski <szumak@virthost.pl>
2024-03-14 13:09:21 +0100

    Create main class and project fix

---
+++ mt_tables_sync/main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+  main.py
+  Author : Jacek 'Szumak' Kotlarski --<szumak@virthost.pl>
+  Created: 14.03.2024, 13:03:47
+  
+  Purpose: main class
+"""
+
+from jsktoolbox.libs.base_data import BData
+
+
+class MtTablesSync(BData):
+    """MtTablesSync main class."""
+
+    def __init__(self) -> None:
+        """Constructor."""
+        super(MtTablesSync, self).__init__()
+
+
+# #[EOF]#######################################################################
---
+++ start-sync.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+  start-sync.py
+  Author : Jacek 'Szumak' Kotlarski --<szumak@virthost.pl>
+  Created: 14.03.2024, 13:00:33
+  
+  Purpose: MikroTik routers address table sync.
+"""
+
+import sys
+from mt_tables_sync.main import MtTablesSync
+
+if __name__ == "__main__":
+    sys.exit(0)
+
+# #[EOF]#######################################################################
